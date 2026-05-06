3a679b15d470c6f973aad58ca0232c107ed0af55
Olivier Delierre <84965894+olivierDelierre@users.noreply.github.com>
2026-05-06 19:18:55 +0200

    Project initialization with SFML and Catch2

---
+++ README.md
@@ -0,0 +1,7 @@
+# CCHIPP-8 - (another) CHIP-8 emulator
+
+A CHIP-8 emulator written in C++ using SFML.
+
+## Quick start
+
+Under construction...
---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+	sf::RenderWindow window(sf::VideoMode({480, 480 }), "CCHIPP-8");
+
+	while (window.isOpen()) {
+		while (const std::optional event = window.pollEvent()) {
+			if (event->is<sf::Event::Closed>()) {
+				window.close();
+			}
+		}
+
+		window.clear();
+		window.display();
+	}
+}
